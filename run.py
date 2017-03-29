26423e5fd989ca8e699bde47ae480d129c0137bc
Caci <caciipakovska@gmail.com>
2017-03-29 03:17:25 +0200

    added defaultEncoding

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 #!flask/bin/python
 from app import app
 
-
+import sys
+if sys.version_info.major < 3:
+    reload(sys)
+sys.setdefaultencoding('utf8')
 
 if __name__ == "__main__":
 	app.run(debug=True)
